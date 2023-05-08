d413c9336717f971556964dfad09c2fdf7290e65
Denery <dorofeevij@gmail.com>
2023-05-08 15:05:10 +0300

    Setup publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,42 @@
 plugins {
     `java-library`
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 allprojects {
     apply(plugin = "java-library")
+}
+
+publishing {
+    repositories {
+        maven("https://mvn.tetratau.net/releases") {
+            credentials(PasswordCredentials::class)
+            name = "tetratau"
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create("installer", MavenPublication::class) {
+            pom {
+                name.set("toki-installer")
+                description.set("A library for installing toki on paper forks or older paper versions")
+                url.set("https://github.com/TetraTau/toki-installer")
+                inceptionYear.set("2023")
+
+                from(components.named("java").get())
+            }
+        }
+    }
+}
+
+tasks {
+    java {
+        withSourcesJar()
+    }
 }
 
 project(":application") {
